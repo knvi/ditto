bcbf153a1c56cc2ca0a71b7e235fb353a36dee22
knvi <hi@kuniewski.codes>
2023-12-29 17:34:33 +0100

    here's what you would look like if you were

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ export function atom<AtomType extends unknown>(
 
       subscribers.forEach((callback) => callback(value));
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   }
 
