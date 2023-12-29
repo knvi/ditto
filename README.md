ffbf5019dc42ca5adf6ee5c6d0e3b9dfd1b6f650
knvi <hi@kuniewski.codes>
2023-12-29 16:32:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# ditto
+
+a tiny state management library for React
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+You need to have the following installed:
+
+- Node.js
+- npm
+- yarn
+
+### Installing
+
+1. Clone the repository:
+```sh
+git clone <repository-url>
+```
+
+2. Navigate to the project directory:
+```sh
+cd <project-directory>
+```
+
+3. Install the dependencies:
+```sh
+yarn install
+```
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+// const atomCount = atom(0)
+// const [count, setCount] = useAtom(atomCount)
+
+// const dataAtom = atom(() => fetch("./data.json").then((res) => res.json()));
+
+import { useSyncExternalStore } from "react";
+
+interface Atom<AtomType> {
+  get: () => AtomType;
+  set: (newValue: AtomType) => void;
+  subscribe: (callback: (newValue: AtomType) => void) => () => void;
+  _subscribers: () => number;
+}
+
+type AtomGetter<AtomType> = (
+  get: <Target>(a: Atom<Target>) => Target
+) => AtomType;
+
+export function atom<AtomType extends unknown> (
+  initialValue: AtomType | AtomGetter<AtomType>
+): Atom<AtomType> {
+  let value: AtomType = 
+    typeof initialValue === "function" ? (null as AtomType) : initialValue;
+
+  const subscribers = new Set<(newValue: AtomType) => void>();
+  const subscribed = new Set<Atom<any>>();
+
+  function get<Target>(atom: Atom<Target>) {
+    let currentValue = atom.get();
+
+    if(!subscribed.has(atom)) {
+      subscribed.add(atom)
+
+      atom.subscribe(function (newValue) {
+        if(currentValue === newValue) {
+          return;
+        }
+
+        currentValue = newValue
+        calculateValue();
+      })
+    }
+
+    return currentValue;
+  }
+
+  async function calculateValue() {
+    const newValue = 
+      typeof initialValue === "function" ? (initialValue as AtomGetter<AtomType>)(get) : value;
+
+    value = (null as AtomType)
+    value = await newValue
+
+    subscribers.forEach((callback) => callback(value));
+  }
+
+  calculateValue();
+
+  return {
+    get: () => value,
+    set: (newValue) => {
+      value = newValue;
+      calculateValue();
+    },
+    subscribe: (callback) => {
+      subscribers.add(callback);
+      return () => {
+        subscribers.delete(callback);
+      };
+    },
+    _subscribers: () => subscribers.size,
+  }
+}
+
+export function useAtom<AtomType>(atom: Atom<AtomType>): [AtomType, (newValue: AtomType) => void] {
+  const state = useSyncExternalStore(atom.subscribe, atom.get);
+
+  return [state, atom.set];
+}
+
+export function useAtomValue<AtomType>(atom: Atom<AtomType>) {
+  return useSyncExternalStore(atom.subscribe, atom.get);
+}
